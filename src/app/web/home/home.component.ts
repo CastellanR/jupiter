03d56bd43c4ebb8236df94993cbf954189806a58
Alan <alan.boglioli@gmail.com>
2017-06-16 18:20:56 -0300

    Fix typo

--- src/app/web/home/home.component.ts
+++ src/app/web/home/home.component.ts
@@ -8,28 +8,28 @@ import { Product } from './../../dashboard/product/product.model';
 })
 export class HomeComponent implements OnInit {
 
-  product: Product[];
+  products: Product[];
 
   constructor() {
-      this.product=[
+      this.products = [
         new Product(
           'IPHONE 7S',
           '001',
-          'resources/photos/i7.jpg',
+          'assets/photos/i7.jpg',
           4,
           300
         ),
         new Product(
           'Samsung Galaxy 8',
           '002',
-          'resources/photos/s8.jpg',
+          'assets/photos/S8.jpg',
           3,
           500
         ),
         new Product(
           'Motorola G5s',
           '003',
-          'resources/photos/g5.jpg',
+          'assets/photos/g5.jpg',
           3,
           200
         )
